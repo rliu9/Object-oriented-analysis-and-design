ceba5e8d367aebaae991f9fa18b84991092adce4
jue <jue@iastate.ed>
2021-04-16 03:09:34 -0500

    Feature5:Cards placed face up on a center pile

--- src/main/java/coms362/cards/slapjack/SlapjackMove.java
+++ src/main/java/coms362/cards/slapjack/SlapjackMove.java
@@ -9,6 +9,7 @@ import coms362.cards.events.remote.HideCardRemote;
 import coms362.cards.events.remote.RemoveFromPileRemote;
 import coms362.cards.events.remote.ShowCardRemote;
 import coms362.cards.events.remote.ShowPlayerScore;
+import coms362.cards.events.remote.UpdatePileRemote;
 import coms362.cards.model.Card;
 import coms362.cards.model.Pile;
 
@@ -18,6 +19,7 @@ public class SlapjackMove implements Move {
 	private Player p;
 	private Pile fromPile;
 	private Pile toPile;
+	private Table ta;
 	
 	public SlapjackMove(Card c, Player p, Pile fromPile, Pile toPile){
 		this.c = c;
@@ -30,8 +32,10 @@ public class SlapjackMove implements Move {
 	public void apply(Table table) {
 		// TODO Auto-generated method stub
 		//Pile center = table.getPile(SlapjackRules.center_pile);
+		this.ta = table;
 		table.removeFromPile(SlapjackRules.center_pile, c);
 		table.addToPile(toPile.selector, c);
+		table.getPile(SlapjackRules.center_pile).setFaceUp(true);
 		table.addToScore(p, 1);
 	}
 
@@ -42,6 +46,7 @@ public class SlapjackMove implements Move {
 		view.send(new AddToPileRemote(toPile, c));
 		view.send(new ShowCardRemote(c));
 		view.send(new ShowPlayerScore(p, null));
+		view.send(new UpdatePileRemote(ta.getPile(SlapjackRules.center_pile)));
 	}
 
 }
